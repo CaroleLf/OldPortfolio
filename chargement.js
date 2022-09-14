cfc5815ebdd83314742445accbfc256b2d50f1e9
Carole Le Flohic <carole.le-flohic@etu.u-bordeaux.fr>
2022-09-14 11:33:18 +0200

    start updating site, change of banner

--- chargement.js
+++ chargement.js
@@ -1,3 +1,6 @@
+
+
+/*
 const loader = document.querySelector('.loader');
 
 window.addEventListener('load', () => {
@@ -5,3 +8,5 @@ window.addEventListener('load', () => {
     loader.classList.add('fondu-out');
 
 })
+
+*/
